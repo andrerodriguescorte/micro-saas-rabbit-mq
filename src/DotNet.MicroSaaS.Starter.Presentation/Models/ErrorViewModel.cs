c34b1294ca970dbfa69ea70a954617081493ac88
André Côrte <andre.corte@fidelizarmais.com.br>
2025-04-11 11:03:48 -0300

    Criado estrutura do projeto para realizar consumer e publisher no rabbitmq.

--- src/DotNet.MicroSaaS.Starter.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNet.MicroSaaS.Starter.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
